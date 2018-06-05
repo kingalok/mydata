cf01b6ec9e924c285f13e7314bdee2fcc247361c
Alok Sharma <mailaloksharma@gmail.com>
2018-06-05 10:26:47 +0530

    Updated finally

--- sendsms.py
+++
@@ -1,6 +0,0 @@
-import requests
-requests.post('https://textbelt.com/text', {
-  'phone': '+91 7995229045',
-  'message': 'Hello world',
-  'key': 'textbelt',
-})
--- test6.py
+++
@@ -1,5 +0,0 @@
-a=[1,2,3]
-print(a[0])
-print(a[1])
-print(a[2])
-print(a[3])
